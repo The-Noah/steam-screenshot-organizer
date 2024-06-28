e559b72244ac6975900afcadee07e5d46658d0c3
The Noah <thenoahbz@gmail.com>
2024-06-28 00:14:39 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,119 @@
+use std::path::PathBuf;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct Game {
+  #[serde(rename = "appID")]
+  app_id: u64,
+  #[serde(rename = "name")]
+  name: String,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct Games {
+  #[serde(rename = "game")]
+  games: Vec<Game>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename = "gamesList")]
+struct GamesList {
+  games: Games,
+}
+
+fn main() {
+  let steam_id = get_steam_id();
+
+  if steam_id.is_none() {
+    println!("Steam ID not found.");
+    return;
+  }
+
+  let library = get_steam_library(&format!("[U:1:{}]", steam_id.unwrap()));
+
+  let screenshots = get_screenshots();
+
+  for screenshot in screenshots {
+    let game_id = screenshot.file_name().unwrap().to_string_lossy().split("_").next().unwrap().parse::<u64>().unwrap();
+
+    for game in library.games.games.iter() {
+      if game.app_id == game_id {
+        // ensure game directory exists
+        let game_directory = get_screenshots_directory().join(&game.name);
+        if !game_directory.exists() {
+          std::fs::create_dir(&game_directory).unwrap();
+        }
+
+        // move screenshot to game directory
+        let new_screenshot = game_directory.join(screenshot.file_name().unwrap());
+        std::fs::rename(&screenshot, &new_screenshot).unwrap();
+
+        println!("Moved {:?} to {:?}", screenshot, game.name);
+
+        break;
+      }
+    }
+  }
+}
+
+fn get_steam_id() -> Option<u64> {
+  let directories = std::fs::read_dir(r#"C:\Program Files (x86)\Steam\userdata"#).unwrap().collect::<Vec<_>>();
+
+  for directory in directories {
+    if !directory.is_ok() {
+      continue;
+    }
+
+    let directory = directory.unwrap();
+
+    if !directory.path().is_dir() {
+      continue;
+    }
+
+    if let Ok(id) = directory.file_name().to_string_lossy().parse::<u64>() {
+      if id == 0 {
+        continue;
+      }
+
+      return Some(id);
+    }
+  }
+
+  None
+}
+
+fn get_steam_library(steam_id3: &String) -> GamesList {
+  let games = reqwest::blocking::get(&format!("https://steamcommunity.com/profiles/{}/games?xml=1", steam_id3))
+    .unwrap()
+    .text()
+    .unwrap();
+
+  serde_xml_rs::from_str(&games).unwrap()
+}
+
+fn get_screenshots() -> Vec<PathBuf> {
+  let mut files = Vec::new();
+
+  for file in std::fs::read_dir(get_screenshots_directory()).unwrap().collect::<Vec<_>>() {
+    if !file.is_ok() {
+      continue;
+    }
+
+    let file = file.unwrap();
+
+    if !file.path().is_file() {
+      continue;
+    }
+
+    files.push(file.path());
+  }
+
+  files
+}
+
+fn get_screenshots_directory() -> PathBuf {
+  PathBuf::from(std::env::home_dir().unwrap().to_string_lossy().to_string())
+    .join("Pictures")
+    .join("Steam Screenshots")
+}
